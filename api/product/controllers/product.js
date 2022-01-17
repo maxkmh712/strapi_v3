573b22036a5e206405b89d97b17d41f72edfe07a
minho <maxkmh712@gmail.com>
2022-01-17 14:43:07 +0900

    [MODIFY] product controllers modified

--- api/product/controllers/product.js
+++ api/product/controllers/product.js
@@ -3,14 +3,17 @@ const { errorHandler } = require("../services/error")
 const { getProductList, getProductDetail } = require("../services/product")
 
 
+// 전체 상품 리스트 조회
 const findProduct = async ctx => {
   const productInfo = ctx.request.query;
 
   try {
     const data = await getProductList(productInfo);
+
     return ctx.send(data, 200);
   } catch (error) {
     const errorInfo = errorHandler(error.message);
+    console.log(errorInfo)
     return ctx.send(errorInfo, 404);
   }
 }
@@ -19,6 +22,7 @@ const findOneProduct = async ctx => {
   const product_id = ctx.params.id;
 
   try{
+    if (!product_id) throw Error("PRODUCT_DOES_NOT_EXIST")
     const product = await getProductDetail(product_id);
     return ctx.send(product, 200);
   } catch (error) {
